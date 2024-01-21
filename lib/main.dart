386f903ffd21e6456485e34b2f00c61b2b41c07c
RakibAhmed <merakib007@gmail.com>
2024-01-21 00:16:18 -0500

    Implemented initial design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +8,112 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Generated Numbers App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const GeneratedNumbersPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class GeneratedNumbersPage extends StatefulWidget {
+  const GeneratedNumbersPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<GeneratedNumbersPage> createState() => _GeneratedNumbersPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GeneratedNumbersPageState extends State<GeneratedNumbersPage> {
+  final _random = Random();
+  int _numbersCount = 5;
+  int _maxNumber = 45;
+  int _numberOfSets = 3;
+  List<List<int>> _generatedSets = [];
 
-  void _incrementCounter() {
+  // Function to generate random sets of numbers
+  void _generateNumbers() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _generatedSets = List.generate(_numberOfSets, (_) {
+        final numbersSet = <int>{};
+        while (numbersSet.length < _numbersCount) {
+          numbersSet.add(_random.nextInt(_maxNumber) + 1);
+        }
+        return numbersSet.toList();
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Lottery Number Generator'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Numbers Count (1...6)',
+                border: const OutlineInputBorder(),
+                hintText: '5',
+              ),
+              keyboardType: TextInputType.number,
+              onChanged: (value) => _numbersCount = int.tryParse(value) ?? 5,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 8.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Max number (1...100)',
+                border: const OutlineInputBorder(),
+                hintText: '45',
+              ),
+              keyboardType: TextInputType.number,
+              onChanged: (value) => _maxNumber = int.tryParse(value) ?? 45,
+            ),
+            const SizedBox(height: 8.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Number of sets (1...5)',
+                border: const OutlineInputBorder(),
+                hintText: '3',
+              ),
+              keyboardType: TextInputType.number,
+              onChanged: (value) => _numberOfSets = int.tryParse(value) ?? 3,
+            ),
+            const SizedBox(height: 8.0),
+            ElevatedButton(
+              onPressed: _generateNumbers,
+              child: const Text('Generate'),
+            ),
+            const SizedBox(height: 8.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _generatedSets.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.yellowAccent,
+                    child: ListTile(
+                      title: Text('Set ${index + 1}: ${_generatedSets[index].join(', ')}'),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Card(
+              color: Colors.lightGreenAccent,
+              child: ListTile(
+                title: const Text('Rakib Ahmed\n301343511r'),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
